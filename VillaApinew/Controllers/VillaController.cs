da0fb7948938f8b24d7119aa88bf6c8d41c37690
Muralidharan Rajendran <m.rajendran3@dedalus.eu>
2023-05-22 17:54:14 +0530

    modalstate

--- VillaApinew/Controllers/VillaController.cs
+++ VillaApinew/Controllers/VillaController.cs
@@ -17,9 +17,9 @@ namespace VillaApinew.Controllers
         }
 
         [HttpGet("{id:int}",Name = "GetVilla")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDto> GetVilla(int id)
         {
             if (id == 0)
@@ -31,6 +31,66 @@ namespace VillaApinew.Controllers
                 return NotFound();
             }
             return Ok(villa);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public ActionResult<VillaDto> Createdata([FromBody] VillaDto villadto)
+        {
+            if(villadto == null)
+            {
+                return BadRequest(villadto);
+            }
+
+           villadto.Id = VillaDatastore.VillaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaDatastore.VillaList.Add(villadto);
+            // return Ok(villadto);
+            //sometimes we nned to expose the data that time createroute will helpfull
+            return CreatedAtRoute("GetVilla", new { villadto.Id }, villadto);
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa=VillaDatastore.VillaList.FirstOrDefault(u=>u.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            VillaDatastore.VillaList.Remove(villa);
+            return NoContent();
+
+
+
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDto villa)
+        {
+            if (villa == null) { 
+                return BadRequest();
+            }
+            var villad=VillaDatastore.VillaList.FirstOrDefault(v => v.Id == id);
+
+            villad.Id=villa.Id;
+            villad.Name=villa.Name;
+
+            return NoContent();
+
+
+
+
         }
     }
 }
--- VillaApinew/Modal/VillaDto.cs
+++ VillaApinew/Modal/VillaDto.cs
@@ -1,8 +1,13 @@
-﻿namespace VillaApinew.Modal
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VillaApinew.Modal
 {
     public class VillaDto
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
